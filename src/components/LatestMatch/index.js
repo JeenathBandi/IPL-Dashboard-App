65561f7a453f6c1da9021b16b3f4487a3bb1ee67
JeenathBandi <167793445+JeenathBandi@users.noreply.github.com>
2024-06-11 08:06:31 +0530

    Update index.js

--- src/components/LatestMatch/index.js
+++ src/components/LatestMatch/index.js
@@ -7,24 +7,41 @@ class LatestMatch extends Component {
     const {latestMatchDetails} = this.props
     const {
       competingTeam,
-      competingTeamLogo,
       date,
-      firstInnings,
-      id,
-      manOfTheMatch,
-      matchStatus,
-      result,
-      secondInnings,
-      umpires,
       venue,
+      result,
+      competingTeamLogo,
+      firstInnings,
+      secondInnings,
+      manOfTheMatch,
+      umpires,
     } = latestMatchDetails
     return (
       <div className="latest-match-container">
         <p className="latest-matches-heading">Latest Matches</p>
         <div className="latest-match-item">
           <div className="first-container">
-            <h1 className="competing-team">{competingTeam}</h1>
+            <p className="competing-team">{competingTeam}</p>
             <p className="date">{date}</p>
+            <p className="venue">{venue}</p>
+            <p className="venue">{result}</p>
+          </div>
+          <div className="middle-container">
+            <img
+              src={competingTeamLogo}
+              alt={`latest match ${competingTeam}`}
+              className="competing-img"
+            />
+          </div>
+          <div className="last-container">
+            <p className="heads">First Innings</p>
+            <p className="heads-content">{firstInnings}</p>
+            <p className="heads">Second Innings</p>
+            <p className="heads-content">{secondInnings}</p>
+            <p className="heads">Man Of The Match</p>
+            <p className="heads-content">{manOfTheMatch}</p>
+            <p className="heads">Umpires</p>
+            <p className="heads-content">{umpires}</p>
           </div>
         </div>
       </div>
